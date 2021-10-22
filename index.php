e476003f34d2ebd48e908d5792c9dd0a85c77810
Jesús Alexis Ramírez Navor <jesusrn221099@gmail.com>
2021-10-21 19:19:29 -0500

    Iniciar Otras Interfaces

--- index.php
+++ index.php
@@ -1,101 +1,94 @@
 <!doctype html>
-<html lang="en">
-
+<html lang="es">
 <head>
-  <title>Inicio</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Inicio</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Bootstrap CSS v5.0.2 -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <!-- Bootstrap Icons -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
-  <!-- Personal CSS -->
-  <link rel="stylesheet" href="css/index.css">
+    <!-- Personal CSS -->
+    <link rel="stylesheet" href="css/index.css">
 
-  <!-- Page Icon -->
-  <link rel="shortcut icon" href="img/logo.png">
+    <!-- Page Icon -->
+    <link rel="shortcut icon" href="img/logo.png">
 
 </head>
 
-<body>
-
-  <section class="sidebar">
+<body class="bg-img">
+<section class="sidebar">
     <div class="d-flex flex-column flex-shrink-1 p-2 bg-light sidebar">
-      <a href="#" class="text-center">
-        <img id="logo" src="img/logo.png">
-      </a>
-      <ul class="nav nav-pills flex-column mt-2 h5">
-        <li class="nav-item mb-2">
-          <a href="index.php" class="nav-link active" aria-current="page">
-            <i class="bi bi-house"></i>
-            Inicio
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="php/crear-pedido.php" class="nav-link link-dark">
-          <i class="bi bi-cart-plus"></i>
-            Crear Pedido
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="php/generar-factura.php" class="nav-link link-dark">
-          <i class="bi bi-calendar2-check"></i>
-            Generar Factura
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="php/facturas.php" class="nav-link link-dark">
-          <i class="bi bi-calendar3"></i>
-            Facturas
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="php/productos.php" class="nav-link link-dark">
-          <i class="bi bi-bag"></i>
-            Productos
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="php/pedidos.php" class="nav-link link-dark">
-          <i class="bi bi-clipboard"></i>
-            Pedidos
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="php/entregar-pedidos.php" class="nav-link link-dark">
-          <i class="bi bi-clipboard-check"></i>
-            Entregar Pedidos
-          </a>
-        </li>
-      </ul>
+        <a href="#" class="text-center">
+            <img id="logo" src="img/logo.png">
+        </a>
+        <ul class="nav nav-pills flex-column mt-2 h5">
+            <li class="nav-item mb-2">
+                <a href="index.php" class="nav-link active" aria-current="page">
+                    <i class="bi bi-house"></i>
+                    Inicio
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="php/crear-pedido.php" class="nav-link link-dark">
+                    <i class="bi bi-cart-plus"></i>
+                    Crear Pedido
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="php/generar-factura.php" class="nav-link link-dark">
+                    <i class="bi bi-calendar2-check"></i>
+                    Generar Factura
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="php/facturas.php" class="nav-link link-dark">
+                    <i class="bi bi-calendar3"></i>
+                    Facturas
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="php/productos.php" class="nav-link link-dark">
+                    <i class="bi bi-bag"></i>
+                    Productos
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="php/pedidos.php" class="nav-link link-dark">
+                    <i class="bi bi-clipboard"></i>
+                    Pedidos
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="php/entregar-pedidos.php" class="nav-link link-dark">
+                    <i class="bi bi-clipboard-check"></i>
+                    Entregar Pedidos
+                </a>
+            </li>
+        </ul>
     </div>
-  </section>
+</section>
 
-  <section class="main">
-    <a href="#">
-      <img id="inicio" class="img-fluid" src="img/inicio.jpeg">
-    </a>
-    <div class="text-over-image" style="width: 100%;">
-      <p class="display-1 welcome" id="welcome" style="padding-left: 22%;">
-        <strong>Bienvenido</strong>
-      </p>
+<section class="main">
+    <div class="container">
+        <p class="text-center display-1 bg-text text-decoration-underline" style=" font-family: Georgia, serif;">
+            Bienvenido
+        </p>
     </div>
-  </section>
+</section>
 
-  <!-- (Optional) - Place this js code after initializing bootstrap.min.js or bootstrap.bundle.min.js -->
-  <script>
-    var triggerEl = document.querySelector('#navId a')
-    bootstrap.Tab.getInstance(triggerEl).show() // Select tab by name
-  </script>
-
-
-  <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<!-- Bootstrap JavaScript Libraries -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
 </body>
 
 
--- php/crear-pedido.php
+++ php/crear-pedido.php
@@ -1,89 +1,129 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
 
 <head>
-  <title>Crear Pedido</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Crear Pedido</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Bootstrap CSS v5.0.2 -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <!-- Bootstrap Icons -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
-  <!-- Personal CSS -->
-  <link rel="stylesheet" href="../css/crear-pedido.css">
+    <!-- Personal CSS -->
+    <link rel="stylesheet" href="../css/crear-pedido.css">
 
-  <!-- Page Icon -->
-  <link rel="shortcut icon" href="../img/logo.png">
+    <!-- Page Icon -->
+    <link rel="shortcut icon" href="../img/logo.png">
 
 </head>
 
 <body>
 
-  <section class="sidebar">
+<section class="sidebar">
     <div class="d-flex flex-column flex-shrink-1 p-2 bg-light sidebar">
-      <a href="#" class="text-center">
-        <img id="logo" src="../img/logo.png">
-      </a>
-      <ul class="nav nav-pills flex-column mt-2 h5">
-        <li class="nav-item mb-2">
-          <a href="../index.php" class="nav-link link-dark" aria-current="page">
-            <i class="bi bi-house"></i>
-            Inicio
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="crear-pedido.php" class="nav-link active">
-          <i class="bi bi-cart-plus"></i>
-            Crear Pedido
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="generar-factura.php" class="nav-link link-dark">
-          <i class="bi bi-calendar2-check"></i>
-            Generar Factura
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="facturas.php" class="nav-link link-dark">
-          <i class="bi bi-calendar3"></i>
-            Facturas
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="productos.php" class="nav-link link-dark">
-          <i class="bi bi-bag"></i>
-            Productos
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="pedidos.php" class="nav-link link-dark">
-          <i class="bi bi-clipboard"></i>
-            Pedidos
-          </a>
-        </li>
-        <li class="nav-item mb-2">
-          <a href="entregar-pedidos.php" class="nav-link link-dark">
-          <i class="bi bi-clipboard-check"></i>
-            Entregar Pedidos
-          </a>
-        </li>
-      </ul>
+        <a href="#" class="text-center">
+            <img id="logo" src="../img/logo.png">
+        </a>
+        <ul class="nav nav-pills flex-column mt-2 h5">
+            <li class="nav-item mb-2">
+                <a href="../index.php" class="nav-link link-dark" aria-current="page">
+                    <i class="bi bi-house"></i>
+                    Inicio
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="crear-pedido.php" class="nav-link active">
+                    <i class="bi bi-cart-plus"></i>
+                    Crear Pedido
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="generar-factura.php" class="nav-link link-dark">
+                    <i class="bi bi-calendar2-check"></i>
+                    Generar Factura
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="facturas.php" class="nav-link link-dark">
+                    <i class="bi bi-calendar3"></i>
+                    Facturas
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="productos.php" class="nav-link link-dark">
+                    <i class="bi bi-bag"></i>
+                    Productos
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="pedidos.php" class="nav-link link-dark">
+                    <i class="bi bi-clipboard"></i>
+                    Pedidos
+                </a>
+            </li>
+            <li class="nav-item mb-2">
+                <a href="entregar-pedidos.php" class="nav-link link-dark">
+                    <i class="bi bi-clipboard-check"></i>
+                    Entregar Pedidos
+                </a>
+            </li>
+        </ul>
     </div>
-  </section>
+</section>
 
-  <section class="main">
-    <p class="display-1">
-        Página para crear los pedidos
-    </p>
-  </section>
+<section class="main">
+    <div class="container text-center">
+      <p class="display-2 text-primary">
+          Crear Pedido
+      </p>
+    </div>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col row">
+                <div class="col-4">
+                    <label>Manteles</label>
+                </div>
+                <div class="col-5">
+                    <input type="text" class="form-control"  value="15">
+                </div>
+            </div>
+            <div class="col">
+                <p>
+                    Subtotal
+                </p>
+            </div>
+            <div class="col">
+                <p>
+                    Pago Completo
+                </p>
+            </div>
+            <div class="col">
+                <label class="form-check-label" for="flexCheckChecked">
+                    Sí
+                </label>
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    No
+                </label>
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
 
-  <!-- Bootstrap JavaScript Libraries -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Bootstrap JavaScript Libraries -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
 </body>
 
 
